d9723b16c44a1cf226b88f0ac278e318e94b3ef2
Eduardo Hernandez <zohanshane@gmail.com>
2023-01-21 07:38:30 -0600

    se hace responsive

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'
 
 const QuoteBox = ({ randomQuote, randomColors, getRandomAll }) => {
   const objectStyle = {
     color: randomColors,
-  };
+  }
   const objectBgStyle = {
     backgroundColor: randomColors,
-  };
+  }
   return (
     <div className="card" style={objectStyle}>
       <header>
@@ -17,18 +17,18 @@ const QuoteBox = ({ randomQuote, randomColors, getRandomAll }) => {
       <div className="card_container">
         <h3 className="card_text">{randomQuote.quote}</h3>
         <p className="card_sub">{randomQuote.author}</p>
-        <div className="btn">
-          <button
-            onClick={getRandomAll}
-            className="card_btn"
-            style={objectBgStyle}
-          >
-            &#62;
-          </button>
-        </div>
+      </div>
+      <div className="btn">
+        <button
+          onClick={getRandomAll}
+          className="card_btn"
+          style={objectBgStyle}
+        >
+          &#62;
+        </button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default QuoteBox;
+export default QuoteBox
